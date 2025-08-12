3fb780ad90f260a8ec3120ccfc7008d15626d292
Sai Mounika <saimounikanedunuri@gmail.com>
2025-08-12 15:55:47 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,19 @@
-// Show/hide top button
-window.onscroll = function () {
-  const topBtn = document.getElementById("topBtn");
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    topBtn.style.display = "block";
-  } else {
-    topBtn.style.display = "none";
-  }
-};
-
-// Scroll to top
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-// Hover sequence logic
-document.querySelectorAll('.hover-sequence').forEach(img => {
-  let interval;
-  let images = img.dataset.images.split(',');
+document.querySelectorAll(".hover-sequence").forEach(img => {
+  let intervalId;
+  let originalSrc = img.src;
+  let images = img.dataset.images.split(",");
   let index = 0;
 
-  img.addEventListener('mouseenter', () => {
-    interval = setInterval(() => {
-      index = (index + 1) % images.length;
+  img.addEventListener("mouseenter", () => {
+    index = 0;
+    intervalId = setInterval(() => {
       img.src = images[index];
-    }, 1000); // change every 1 second
+      index = (index + 1) % images.length;
+    }, 800);
   });
 
-  img.addEventListener('mouseleave', () => {
-    clearInterval(interval);
-    img.src = images[0]; // reset to first
-    index = 0;
+  img.addEventListener("mouseleave", () => {
+    clearInterval(intervalId);
+    img.src = originalSrc;
   });
 });
